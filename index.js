73b83cf1aab11ec852388942b8ad6e4ae19284a2
metalex9 <alexbainter@gmail.com>
2018-09-04 21:38:31 -0500

    Use browser window instead of notification

--- index.js
+++ index.js
@@ -1,67 +1,63 @@
-const { app, Menu, Tray } = require('electron');
+const { app, Menu, Tray, BrowserWindow } = require('electron');
 const notifier = require('node-notifier');
 
-const eyeInterval = 21;
-const breakInterval = 60;
+let win;
+let running = true;
 
-const reminders = [
-  {
-    name: '20-20-20 Rule',
-    message: 'Look at something at least 20 feet away for 20 seconds',
-    intervalTimeMinutes: eyeInterval,
-    minutesRemaining: eyeInterval,
-  },
-  {
-    name: 'Hourly Breaks',
-    message: 'Take a 5 minute break',
-    intervalTimeMinutes: breakInterval,
-    minutesRemaining: breakInterval,
-  },
-];
+const tick = timeInMinutes => {
+  if (running) {
+    console.log(timeInMinutes);
+    if (timeInMinutes > 0) {
+      let message;
+      if (timeInMinutes % 60 === 0) {
+        message = 'Take a break.';
+      } else if (timeInMinutes % 20 === 0) {
+        message = 'Look at something 20 feet away for 20 seconds.';
+      }
+      if (message) {
+        running = false;
+        win = new BrowserWindow({
+          width: 400,
+          height: 200,
+          alwaysOnTop: true,
+          frame: false,
+        });
+        win.loadURL('file://' + __dirname + '/alert.html');
+        win.text = message;
+        win.on('close', e => {
+          running = true;
+          tick(timeInMinutes + 1);
+        });
+        return;
+      }
+    }
+    setTimeout(() => tick(timeInMinutes + 1), 100);
+  }
+};
 
-const getLabel = ({ name, minutesRemaining }) =>
-  `${name} (next in ${minutesRemaining}m)`;
+const makeTurnOn = trayIcon => () => {
+  running = true;
+  tick(0);
+  trayIcon.setContextMenu(
+    Menu.buildFromTemplate([
+      { label: 'Turn Off', click: makeTurnOff(trayIcon) },
+    ])
+  );
+};
 
-const startInterval = reminder => {
-  reminder.interval = setInterval(() => {
-    notifier.notify({
-      message: reminder.message,
-      title: 'Wellness Minder',
-      timeout: 60,
-    });
-  }, reminder.intervalTimeMinutes * 60 * 1000);
+const makeTurnOff = trayIcon => () => {
+  running = false;
+  trayIcon.setContextMenu(
+    Menu.buildFromTemplate([{ label: 'Turn On', click: makeTurnOn(trayIcon) }])
+  );
 };
 
 let trayIcon;
-const makeMenuTemplate = () =>
-  reminders.map(reminder => ({
-    label: getLabel(reminder),
-    type: 'checkbox',
-    checked: true,
-    click: ({ checked }) => {
-      if (checked) {
-        startInterval(reminder);
-      } else {
-        clearInterval(reminder.interval);
-      }
-    },
-  }));
-
 app.on('ready', () => {
   trayIcon = new Tray('./face.png');
   trayIcon.setToolTip('Wellness Minder');
-  setInterval(() => {
-    reminders.forEach((reminder, i) => {
-      const { minutesRemaining } = reminder;
-      reminder.minutesRemaining =
-        minutesRemaining === 1
-          ? reminder.intervalTimeMinutes
-          : minutesRemaining - 1;
-    });
-    trayIcon.setContextMenu(Menu.buildFromTemplate(makeMenuTemplate()));
-  }, 60 * 1000);
-  reminders.forEach(reminder => {
-    startInterval(reminder);
-  });
-  trayIcon.setContextMenu(Menu.buildFromTemplate(makeMenuTemplate()));
+  makeTurnOn(trayIcon)();
+});
+app.on('window-all-closed', e => {
+  e.preventDefault();
 });
